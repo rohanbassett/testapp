430dd62b8857c4f127c48fa231371eb0a8f1a750
Rohan Bassett <rohan.bassett@me.com>
2013-08-27 05:36:08 +0000

    Articles scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20130827051840) do
 
-  create_table "fiction", force: true do |t|
+  create_table "articles", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "articles2", force: true do |t|
     t.string "author", limit: 32
     t.string "title",  limit: 64
   end
